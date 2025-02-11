4da5f4d1c29b3c8010bae9267f6ce19cc8c9edf4
Ruiqi Diao <ruiqi.richie.diao2020@gmail.com>
2025-02-11 14:00:40 +1100

    change base in config and add minHeight

--- src/App.jsx
+++ src/App.jsx
@@ -6,64 +6,6 @@ import lucy from "./assets/lucy.jpg";
 import city from "./assets/city.jpg";
 import suitcase from "./assets/suitcase_sunshine.jpg";
 
-// function App() {
-
-//   // return (<div>
-//   //   <div className="bg-image img1"></div>
-//   //   <div className="bg-image img2"></div>
-//   //   <div className="bg-image img3"></div>
-//   //   <div className="bg-image img4"></div>
-//   //   <div className="bg-image img5"></div>
-
-//   //   <div className="bg-text">TEXT</div>
-//   // </div>);
-
-//   // // const backgrounds = [suitcase, uluru, marcus, lucy, city]; // List of backgrounds
-//   // // const [currentBackground, setCurrentBackground] = useState(backgrounds[0]);
-
-//   // // useEffect(() => {
-//   // //   const handleScroll = () => {
-//   // //     const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
-//   // //     const scrollPosition = window.scrollY; // Current scroll position
-//   // //     const section = Math.floor((scrollPosition / scrollHeight) * backgrounds.length);
-
-//   // //     setCurrentBackground(backgrounds[section] || backgrounds[backgrounds.length - 1]);
-//   // //   };
-
-//   // //   window.addEventListener("scroll", handleScroll);
-//   // //   return () => window.removeEventListener("scroll", handleScroll);
-//   // // }, []);
-
-//   // // return (
-//   // //   <div className="container" style={{ backgroundImage: `url(${currentBackground})` }}>
-//   // //     <div className="content">
-//   // //       <div className="spacer"></div>
-//   // //     </div>
-//   // //   </div>
-//   // // );
-
-//   const images = [city, suitcase, lucy, uluru, marcus]; // List of background images
-//   const [bgIndex, setBgIndex] = useState(0);
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       const scrollPosition = window.scrollY; // Current scroll position
-//       const sectionHeight = window.innerHeight; // Each section is one full viewport height
-//       const index = Math.floor(scrollPosition / sectionHeight);
-//       setBgIndex(index < images.length ? index : images.length - 1);
-//     };
-
-//     window.addEventListener("scroll", handleScroll);
-//     return () => window.removeEventListener("scroll", handleScroll);
-//   }, []);
-
-//   return (
-//     <div className="container" style={{ backgroundImage: `url(${images[bgIndex]})` }}>
-//       <div className="spacer"></div>
-//     </div>
-//   );
-// }
-
 function App() {
   const images = [marcus, city, suitcase, lucy, uluru];
   // const images = [lucy, marcus];
@@ -74,7 +16,10 @@ function App() {
         <div
           key={index}
           className="section"
-          style={{ backgroundImage: `url(${image})` }}
+          style={{ 
+            backgroundImage: `url(${image})`,
+            minHeight: '100vh' 
+          }}
         >
           {/* <div className="bg-text"></div> */}
         </div>
